882287b123a6c5c6075d23a10a400e5fa78ce5ca
the-sankari <kajol.sd777@gmail.com>
2026-07-26 20:49:56 +0300

    Conditions and conditional execution lab

---
+++ ciso_netacad/PE1/module_3/exercises/snake_1.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# choose the larger number
+if number1 > number2:
+    largernumber = number1
+else:
+    largernumber = number2
+
+# print the larger number
+print("The larger number is:", largernumber) 
