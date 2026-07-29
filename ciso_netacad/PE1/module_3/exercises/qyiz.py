d4887318d460fafec5fe694d31f589ccdc2b016d
the-sankari <kajol.sd777@gmail.com>
2026-07-29 23:29:07 +0300

    section quiz 4.

--- ciso_netacad/PE1/module_3/exercises/qyiz.py
+++ ciso_netacad/PE1/module_3/exercises/qyiz.py
@@ -17,10 +17,20 @@
 ####################################
 
 # Question 3: Create a program with a for loop and a break statement. The program should iterate over characters in an email address, exit the loop when it reaches the @ symbol, and print the part before @ on one line.
-email = input("Enter your email address: ")
-for ch in email:
-    if ch == "@":
-        break
-    print(ch, end="")
-print()  # Move to the next line after printing the part before @
+# email = input("Enter your email address: ")
+# for ch in email:
+#     if ch == "@":
+#         break
+#     print(ch, end="")
+# print()  # Move to the next line after printing the part before @
 
+########################################
+# Question 4: Create a program with a for loop and a continue statement. The program should iterate over a string of digits, replace each 0 with x, and print the modified string to the screen.
+string_of_digits = input("Enter a string of digits: ")
+
+for digit in string_of_digits:
+    if digit == '0':
+        print('x', end="")
+        continue
+    print(digit, end="") 
+print()  # Move to the next line after printing the modified string
