7d4888d9f99811dbfa9f8c8368f9e2fca706e351
the-sankari <kajol.sd777@gmail.com>
2026-07-29 23:20:57 +0300

    section quiz 2.

--- ciso_netacad/PE1/module_3/exercises/qyiz.py
+++ ciso_netacad/PE1/module_3/exercises/qyiz.py
@@ -1,4 +1,15 @@
 # Question 1: Create a for loop that counts from 0 to 10, and prints odd numbers to the screen
-for i in range(1,11):
-    if i % 2 != 0:
-        print(i)
+# for i in range(1,11):
+#     if i % 2 != 0:
+#         print(i)
+
+
+######################
+
+# Question 2: Create a while loop that counts from 0 to 10, and prints odd numbers to the screen.
+
+x = 1
+while x < 11:
+    if x % 2 != 0:
+        print(x)
+    x +=1
