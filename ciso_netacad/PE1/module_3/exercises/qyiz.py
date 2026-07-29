09c1338dc1a4316b216b07ea3be14916c6c6f596
the-sankari <kajol.sd777@gmail.com>
2026-07-29 23:25:44 +0300

    section quiz 3.

--- ciso_netacad/PE1/module_3/exercises/qyiz.py
+++ ciso_netacad/PE1/module_3/exercises/qyiz.py
@@ -8,8 +8,19 @@
 
 # Question 2: Create a while loop that counts from 0 to 10, and prints odd numbers to the screen.
 
-x = 1
-while x < 11:
-    if x % 2 != 0:
-        print(x)
-    x +=1
+# x = 1
+# while x < 11:
+#     if x % 2 != 0:
+#         print(x)
+#     x +=1
+
+####################################
+
+# Question 3: Create a program with a for loop and a break statement. The program should iterate over characters in an email address, exit the loop when it reaches the @ symbol, and print the part before @ on one line.
+email = input("Enter your email address: ")
+for ch in email:
+    if ch == "@":
+        break
+    print(ch, end="")
+print()  # Move to the next line after printing the part before @
+
