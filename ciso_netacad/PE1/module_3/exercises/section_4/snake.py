049bf96754827b1dfcd2d301504cc68b3e87a193
the-sankari <kajol.sd777@gmail.com>
2026-07-31 02:07:58 +0300

    Making use of lists

--- ciso_netacad/PE1/module_3/exercises/section_4/snake.py
+++ ciso_netacad/PE1/module_3/exercises/section_4/snake.py
@@ -9,3 +9,14 @@ print("New list contents:", numbers)  # Printing current list contents.
 print("\nList length:", len(numbers))  # Printing the new value of the second element.
 del numbers[3]  # Deleting the fourth element.
 print("List contents after deleting the fourth element:", numbers)  # Printing list contents after deletion
+
+my_list = []
+for i in range(6):
+    my_list.append(i + 1)  # Adding numbers 1 through 6 to the list.
+print("\nList contents after adding numbers 1 through 6:", my_list)  # Printing list contents after adding numbers 1 through 6.
+
+#####################
+total = 0
+for i in range(len(my_list)):
+    total += my_list[i] # Adding each element of the list to the total.
+print("Total:", total)  # Printing the total.
